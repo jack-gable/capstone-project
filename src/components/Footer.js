930876cfc43141a84f88ee2665003cef3029a99a
Jack Gable <81646353+jack-gable@users.noreply.github.com>
2024-05-04 13:40:14 -0700

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+function Footer() {
+	return (
+		<footer>
+			<img href="" alt="" />
+			<ul>
+				<li>Doormat navigation</li>
+				<li>
+					<a href="/">Home</a>
+				</li>
+				<li>
+					<a href="/">About</a>
+				</li>
+				<li>
+					<a href="/">Menu</a>
+				</li>
+				<li>
+					<a href="/">Reservations</a>
+				</li>
+				<li>
+					<a href="/">Online Order</a>
+				</li>
+				<li>
+					<a href="/">Login</a>
+				</li>
+			</ul>
+			<ul>
+				<li>Contact</li>
+				<li>address</li>
+				<li>phone number</li>
+				<li>email</li>
+			</ul>
+			<ul>
+				<li>Social</li>
+				<li>address</li>
+				<li>phone number</li>
+				<li>email</li>
+			</ul>
+		</footer>
+	);
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Nav from "./Nav";
+
+function Header() {
+	return (
+		<header>
+			<img src="logo.svg" alt="logo" />
+			<Nav />
+		</header>
+	);
+}
+
+export default Header;
